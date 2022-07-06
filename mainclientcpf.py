dfd4a215cbbd5ac107fd95d29a184c3b8de5860d
Ramon Almeida <ramongoncalves76@gmail.com>
2022-07-06 19:54:07 -0300

    Added client cpf

---
+++ mainclientcpf.py
@@ -0,0 +1,41 @@
+from clientserviceutils.client_req_utils import get_servname_service_info, get_service_operation
+
+
+def main():
+
+    BUFFER_LEN = 1024
+    HOST_SERVER_NAME = 'localhost'
+    PORT_SERVER_NAME = 20467
+
+    print('-------- CPF VALIDADOR --------\n')
+
+    while True:
+
+        cpf = input('Digite o CPF: ')
+
+        data_res = get_servname_service_info(
+            HOST_SERVER_NAME, PORT_SERVER_NAME, 'cpfservice')
+
+        if data_res is None:
+            print(f' - Servidor não encontrado ou Inativo\n')
+            continue
+
+        host_service, port_service = data_res['host'], data_res['port']
+
+        print('\nDados:')
+        print(
+            f' - Info. Servidor da Requisição: host({host_service}) / port({port_service})')
+
+        data_cpf = {'cpf': cpf}
+
+        data_cpf_res = get_service_operation(
+            host_service, port_service, data_cpf)
+
+        print(' - Resultado')
+        print(f'   - {cpf}: {data_cpf_res["res"]}')
+
+        print('\n')
+
+
+if __name__ == '__main__':
+    main()
